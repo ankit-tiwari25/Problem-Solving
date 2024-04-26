bdc9d0d5d12d83cfcb6c7f2811766948e1e30e75
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-04-26 07:50:14 +0530

    Appraoch 2 : Reversing array elements (Most optimal appraoch)

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -1,13 +1,18 @@
 package Striver.Arrays;
 
 //Problem 189 [Medium]  : Rotate Array k times
+/*
+ * Appraoch 1 : Directly using index : T.C=O(n)  S.C = O(n)
+ * Appraoch 2 : Reversing elements   : T.C =O(n) S.C = O(1) -> Most Optimal Appraoch
+ */
 
 public class RotateArray {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7};
-        int k = 9;
+        int k = 5;
 
-        int rotatedArray[] = rotateArrayApproachOne(arr, k);
+        // int rotatedArray[] = rotateArrayApproachOne(arr, k);
+        int rotatedArray[] = rotateArrayApproachTwo(arr, k);
         display(rotatedArray);
 
     }
@@ -33,10 +38,48 @@ public class RotateArray {
 
         return arr;
     }
+
+    public static int[] rotateArrayApproachTwo(int nums[], int k){
+        // Without using extra space : Reversing array elements
+        /* 1. Reverse entire array
+         * 2. Reverse 0-k elements
+         * 3. Reverse k - n elements
+         */
+        int n = nums.length;
+        k = k % n;
+
+        //1. Reversing entire elements
+        for(int i = 0, j = n -1; i <= j; i++, j--){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+
+        display(nums);
+        //2. Reversing 0-k elements
+        for(int i = 0, j = k-1; i <= j; i++, j--){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+                display(nums);
+
+        //3. Reversing k - n elements
+        for(int i = k, j = n -1; i <= j; i++, j--){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        display(nums);
+
+
+        return nums;
+    }
     public static void display(int arr[]){
         for(int i : arr){
             System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     

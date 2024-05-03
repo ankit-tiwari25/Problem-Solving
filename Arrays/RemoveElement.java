d3e2818f8fe2982fb79ba744a928366852e91d28
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-05-03 18:39:49 +0530

    27. Remove Element

--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -1,10 +1,10 @@
 package Striver.Arrays;
 
-
+// Problem 27. [Easy] Remove Element
 public class RemoveElement {
     public static void main(String[] args) {
         int arr[] = {0,1,2,2,3,0,4,2};
-        int val = 5;
+        int val = 2;
         System.out.println(removeElement(arr, val));
     }
     public static int removeElement(int[] nums, int val){
@@ -13,6 +13,7 @@ public class RemoveElement {
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != val){
                 nums[i] = nums[j];
+                i++;
             }
         }
         return i;

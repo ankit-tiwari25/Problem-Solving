d1717caf970ebfb48f8d9ecc1dbb2429adade58b
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-05-09 18:54:18 +0530

    80. Remove Duplicates from Sorted Array II

---
+++ TwoPointer/RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,28 @@
+package Striver.TwoPointer;
+//80. Remove Duplicates from Sorted Array II
+public class RemoveDuplicatesfromSortedArrayII {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,2,3};
+        display(arr);
+        System.out.println(removeDuplicates(arr));
+
+    }
+    public static int removeDuplicates(int[] nums) {
+        //Two pointer approach
+          int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+    public static void display(int arr[]){
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+}

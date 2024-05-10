0ff61eda4bab873642945649f8362f5de0307355
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-05-10 21:05:57 +0530

    374. Guess Number Higher or Lower

---
+++ BinarySearch/GuessNumberHigherorLower.java
@@ -0,0 +1,28 @@
+package Striver.BinarySearch;
+//374. Guess Number Higher or Lower
+public class GuessNumberHigherorLower {
+    public static void main(String[] args) {
+        System.out.println(guessNumberHigherorLower(10));
+    }
+    public static int guessNumberHigherorLower(int n){
+        int i = 1;
+        int j = n;
+
+        while(i<=j){
+            int mid = i + (j-i)/2;
+
+            int res = guess(mid,n);
+            if( res == 0) return mid;
+            if( res == 1) i = mid + 1;
+            else j = mid-1;
+        }
+        return -1;
+    }
+    public static int guess(int mid, int n){
+        if( (n/2) == mid){
+            return 0;
+        }
+        return -1;
+    }
+
+}

7cc9ca76599d92e13362b92d5b8d576e4616087c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-05-20 12:51:13 +0530

    567.[Medium] Permutation in String

---
+++ SlidingWindow/PermutationinString.java
@@ -0,0 +1,45 @@
+package Striver.SlidingWindow;
+// 567.[Medium] Permutation in String
+// TODO
+public class PermutationinString {
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1, s2));
+    }
+    public static boolean checkInclusion(String s1, String s2) {
+     
+        //if s1 itself is large return false
+               if(s1.length()>s2.length()){
+                   return false;
+               } 
+               //store freq of s1 and s2
+               int[]freq=new int[26];
+               //prepare freq length till s1 length,for s1 character add 1 for s2 char subs 1
+               for(int i=0;i<s1.length();i++){
+                   freq[s1.charAt(i)-'a']+=1;
+                   freq[s2.charAt(i)-'a']-=1;
+               }
+               //loop till exhaust at that time we'll compare total s2.length()characters with s1.length() characters
+               for(int i=0;i<s2.length()-s1.length();i++){
+                   //chaeck if s1 & s2 are anagram of each other
+                   if(checkAnagram(freq)){
+                       return true;
+                   }
+                // if not updated frequently, map to remove s2 first character and add s2 next character
+                freq[s2.charAt(i)-'a']+=1;
+                freq[s2.charAt(i+s1.length())-'a']-=1;
+               } 
+               //chaeck for last comparison
+               return checkAnagram(freq);
+            }
+            //check if freq map has all zero, it will be true for anagram
+            private static boolean checkAnagram(int[]freq){
+                for(int i=0;i<freq.length;i++){
+                    if(freq[i]!=0){
+                        return false;
+                    }
+                }
+                return true;   
+            }
+}
